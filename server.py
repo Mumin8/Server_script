41739af6d33e6d62d5fd7867768edf4887afd87d
Mumin8 <alhassanmumin8@gmail.com>
2023-06-28 22:17:17 +0000

    reread on query

--- server.py
+++ server.py
@@ -17,13 +17,20 @@ def find_path() -> str:
     return file_path
 
 
-def handle_client(client_socket: object, file_path: str):
+def handle_client(
+        client_socket: object, file_path: str, reread_on_query: bool):
     '''
-    handle_client: it handles the client clear text string
+    handle_client:
+        it handles the client clear text string
 
     args:
         client_socket: the object of the client socket
-        file_path: the path to the file
+
+        file_path:
+            the path to the file to read from
+
+        reread_on_query:
+            the boolean to determine whether on not to read from the file again
     '''
 
     while True:
@@ -34,29 +41,48 @@ def handle_client(client_socket: object, file_path: str):
         # decode the received string from the client
         the_string = data.decode('utf-8')
 
-        with open(file_path.lstrip('linuxpath=/').rstrip('\n'), 'r') as file:
-            for line in file:
-                if line.strip() == the_string:
+        if reread_on_query:
+            # checks whether the query string is in the file and read ones
+            with open(file_path.lstrip('linuxpath=/').rstrip('\n'),
+                      'r') as file:
+
+                if the_string in file.read():
                     resp = b"STRING EXISTS"
-                    client_socket.send(resp)
-                    break
-            else:
-                resp = b'STRING DOES NOT EXIST'
-                client_socket.send(resp)
+                else:
+                    resp = b"STRING DOES NOT EXIST"
+        else:
+            # goes through the file to look for a match
+            with open(file_path.lstrip('linuxpath=/').rstrip('\n'),
+                      'r') as file:
+
+                for line in file:
+                    if line.strip() == the_string:
+                        resp = b"STRING EXISTS"
+                        break
+                else:
+                    resp = b'STRING DOES NOT EXIST'
+
+        client_socket.send(resp)
 
     # Close the client socket
     client_socket.close()
 
 
-def start_server(host: str, port: int, file_path: str):
+def start_server(host: str, port: int, file_path: str, reread_on_query: bool):
     '''
     start_server: binds a port and responds to unlimited amount of
     concurrent connections
 
     args:
-        host: a string representing the host or ip address of the server
-        port: an integer representing the port number of the server to bind
-    '''
+        host:
+            a string representing the host or ip address of the server
+        port:
+            an integer representing the port number of the server to bind
+        file_path:
+                the path to the file to read from
+        reread_on_query:
+                boolean to check whether to reread a file or not
+        '''
 
     # Create a socket object
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -76,7 +102,8 @@ def start_server(host: str, port: int, file_path: str):
         # Start a new thread to handle the client
         client_th = threading.Thread(
                     target=handle_client,
-                    args=(client_sock, file_path,))
+                    args=(client_sock, file_path, reread_on_query))
+
         client_th.start()
 
 
@@ -84,6 +111,7 @@ if __name__ == '__main__':
     # Specify the host and port to bind the server to
     HOST = '127.0.0.1'
     PORT = 8000
+    REREAD_ON_QUERY = True
     # Start the server
     file_path = find_path()
-    start_server(HOST, PORT, file_path)
+    start_server(HOST, PORT, file_path, REREAD_ON_QUERY)
