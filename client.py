dcf338ebeaec587adc8dd8860a4bff7d93af417b
Mumin8 <alhassanmumin8@gmail.com>
2023-07-02 17:16:24 +0000

    restructure

--- client.py
+++ client.py
@@ -24,8 +24,10 @@ def search_string(query: str) -> str:
                 secure_soc.send(query.encode())
                 res = secure_soc.recv(1024).decode()
                 if res == 'STRING EXISTS':
+                    print(f'{res}')
                     return res
                 else:
+                    print(f'{res}')
                     return res
 
     except socket.error as e:
@@ -34,10 +36,13 @@ def search_string(query: str) -> str:
         print(f"SSL error occurred: {str(e)}")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
-
-
-search_string('3;0;1;28;0;7;5;0;')
-
+    except socket.error as e:
+        print(f'{e}')
+    except ssl.SSLError as e:
+        print(f"SSL error occurred: {str(e)}")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+search_string('17;0;1;26;0;19;5;0;')
 '''
 file_sizes = [10000, 50000, 100000, 500000, 1000000]
 
@@ -48,5 +53,7 @@ for size in file_sizes:
     search_string(query)
     end_time = time.time()
     execution_time = end_time - start_time
-    print(f"File size: {size} | Execution time: {execution_time} seconds")
-    '''
+    print()
+    print()
+    print(f'File size: {size}')
+    print(f"Execution time: {execution_time} seconds")'''
