31b0c5cb60f855a256edc27d2f27e91d5e7e5bf7
Mumin8 <alhassanmumin8@gmail.com>
2023-07-10 13:23:56 +0000

    client

--- client.py
+++ client.py
@@ -1,27 +1,26 @@
 import socket
 import ssl
 import time
+import argparse
+from server import server_config
 
-HOST = '127.0.0.1'
-PORT = 8000
 
-
-def search_string(query: str) -> str:
-
+def search_string(query_string: str) -> str:
     '''
     search_string: it is a client for the server application
 
     args:
-        query: the query string for the search
+        query_string: the query string for the search
     '''
 
     context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+
     context.check_hostname = False
     context.verify_mode = ssl.CERT_NONE
     try:
         with socket.create_connection((HOST, PORT)) as soc:
             with context.wrap_socket(soc, server_hostname=HOST) as secure_soc:
-                secure_soc.send(query.encode())
+                secure_soc.send(query_string.encode())
                 res = secure_soc.recv(1024).decode()
                 if res == 'STRING EXISTS':
                     print(f'{res}')
@@ -42,13 +41,14 @@ def search_string(query: str) -> str:
         print(f"SSL error occurred: {str(e)}")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
-search_string('17;0;1;26;0;19;5;0;')
+
+
 '''
 file_sizes = [10000, 50000, 100000, 500000, 1000000]
 
 
 for size in file_sizes:
-    query = '17;0;1;26;0;19;5;0;' * (size // 16)
+    query = query_string * (size // 16)
     start_time = time.time()
     search_string(query)
     end_time = time.time()
@@ -56,4 +56,23 @@ for size in file_sizes:
     print()
     print()
     print(f'File size: {size}')
-    print(f"Execution time: {execution_time} seconds")'''
+    print(f"Execution time: {execution_time} seconds")
+'''
+if __name__ == "__main__":
+    # Create an argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--query", type=str, help="Query string")
+
+    # Parse the command-line arguments
+    args = parser.parse_args()
+
+    # Access the query string
+    query = args.query
+
+    # Get the server configuration
+    server = server_config()
+    HOST = server["host"]
+    PORT = server["port"]
+
+    # Perform the search
+    search_string(query)
