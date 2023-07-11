89331401b76da043546d4e8c5096ab2fb36ca7a9
Mumin8 <alhassanmumin8@gmail.com>
2023-07-11 11:14:40 +0000

    pytest

--- test_server.py
+++ test_server.py
@@ -29,29 +29,47 @@ def test_find_path():
 
     assert path is not None and path != "", "The path must exist"
 
-    assert "DEFAULT" not in config or 'linuxpath' not in config['DEFAULT']
+    assert "DEFAULT" not in config or \
+        'linuxpath' not in config['DEFAULT'], "DEFAULT in configuration file"
+
+
+def test_server_config():
+    # Perform the setup for the test case if needed
+
+    # Call the function to get the server configuration
+    config = server_config()
+
+    # Perform assertions to verify the expected values
+    assert isinstance(config, dict), "server_config returns a dictionary"
+    assert 'host' in config, "host is in configuration file"
+    assert 'port' in config, "port is in configuration file"
+    assert 'REREAD_ON_QUERY' in config, "REREAD_ON_QUERY exists"
+    assert 'USE_SSL' in config, "USE_SSL exists"
+    assert isinstance(config['USE_SSL'], dict), "USE_SSL is a dictionary"
+    assert 'ssl' in config['USE_SSL'], "ssl exist in USE_SSL dictionary"
+    assert 'certfile' in config['USE_SSL']
+    assert 'keyfile' in config['USE_SSL']
+    assert isinstance(config['REREAD_ON_QUERY'], bool)
+    assert type(config['port']) is int
+    assert type(config['host']) is str
 
 
 def test_start_server():
     '''
     test_start_server: this starts the server
-    
-    FILE_PATH = server.find_path()
-    REREAD_ON_QUERY = True
-    USE_SSL = {
-            'ssl': True,
-            'certfile': 'server.crt',
-            'keyfile': 'server.key'
-            }
     '''
+    started = None
     FILE_PATH = find_path()
     server = server_config()
-    # Start the server in a separate thread
-    server_thread = threading.Thread(
+    try:
+        server_thread = threading.Thread(
             target=start_server,
-            args=(server['host'], server['port'], FILE_PATH, server['REREAD_ON_QUERY'], server['USE_SSL']))
-    server_thread.start()
-
+            args=(server['host'], server['port'], FILE_PATH,
+                  server['REREAD_ON_QUERY'], server['USE_SSL']))
+        server_thread.start()
+        started = True
+    except Exception:
+        started = False
     # Wait for the server to start
     time.sleep(1)
 
@@ -61,5 +79,7 @@ def test_start_server():
     # Stop the server
     server_thread.join(timeout=1)
 
-    # Assert that the server thread has stopped
-    assert server_thread.is_alive()
+    
+    assert server_thread.is_alive(), "Assert that the server is alive"
+    
+    assert started, "Assert that the server started"
